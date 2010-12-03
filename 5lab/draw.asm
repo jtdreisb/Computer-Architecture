9e44296cde01ab3e4ad58eecc3ae8af280cd0496
Jason Dreisbach <jtdreisb@127x33.csc.calpoly.edu>
2010-12-03 15:26:09 -0800

    yay it draws

--- 5lab/draw.asm
+++ 5lab/draw.asm
@@ -98,13 +98,13 @@ circle: and $t0, $0, $0
         sub $t2, $a2, $t2 # g = 3-r  
         sub $t2, $a2, $t2 # g = 3-2*r  
   
-        addi $t3, $t3, 10 # diagonalInc = 10  
+        addi $t3, $0, 10 # diagonalInc = 10  
         sub $t3, $a2, $t3 # diagonalInc = 10 - r  
         sub $t3, $a2, $t3 # diagonalInc = 10 - 2*r  
         sub $t3, $a2, $t3 # diagonalInc = 10 - 3*r  
         sub $t3, $a2, $t3 # diagonalInc = 10 - 4*r  
   
-        addi $t4, $t4, 6 # rightInc = 6  
+        addi $t4, $0, 6 # rightInc = 6  
   
 while:  addi $t7, $t1, 1  
         slt $t7, $t0, $t7 # x<= y  
@@ -198,144 +198,98 @@ after:  addi $t4, $t4, 4  #rightInc += 4
   
 end: jr $ra         
   
-  
-#a0 = first point  
-#a1 = second point  
-#t9 = location to start storing  
-  
-plot:   sw $a0, 0($t9)  
-        addi $t9, $t9, 1  
-        sw $a1, 0($t9)   
-        jr $ra  
-#$a0 - x  
-#$v0 - return absolute val  
-abs: addi $t1, $0, 1  
-    addi $t2, $0, 31  
-loop: add $t1, $t1, $t1 #multiply by 2  
-    addi $t2, $t2, -1  
-    bne  $0, $t2, loop  
-#now we have a mask  
-    and $t3, $t1, $a0 #check for negativity  
-    bne $0, $t3, invert  
-    add $v0, $a0, $0  
-    jr $ra  
-invert: sub $v0, $a0, $0  
-    jr $ra  
-  
-# Draw Line    
-#$a0 - x0  
-#$a1 - y0  
-#$a2 - x1  
-#$a3 - y1  
-#$s4 - st  
-#$s5 - dx  
-#$s6 - dy  
-#$s7 - error  
-#$t0 - x  
-#$t1 - y  
-#$t2 - ystep  
-line: add $s0, $0, $a0  
-    add $s1, $0, $a1  
-    add $s2, $0, $a2  
-    add $s3, $0, $a3  
-    sub $t1, $a1, $a3  
-    sub $t2, $a0, $a2  
-  
-#absolute val of y1-y0  
-    add $a0, $0, $t1  
-    addi $sp, $sp, -1  
-    sw $ra, 0($sp)  
-    jal abs  
-    lw $ra, 0($sp)  
-    addi $sp, $sp, 1  
-    add $t1, $0, $v0  
-  
-#absolute val of x1-x0  
-    add $a0, $0, $t2  
-    addi $sp, $sp, -1  
-    sw $ra, 0($sp)  
-    jal abs  
-    lw $ra, 0($sp)  
-    addi $sp, $sp, 1  
-    add $t2, $0, $v0  
-  
-#test dx < dy  
-    slt $t0, $t2, $t1  
-    bne $0, $t0, setst  
-    addi $s4, $0, 1   
-    j swapxy  
-setst: add $s4, $0, $0   
-swapxy: bne $0, $s4, doswapxy  
-    j swapx  
-doswapxy: add $s0, $s0, $s1  
-        sub $s1, $s1, $s0  
-        sub $s0, $s1, $s0  
-  
-        add $s2, $s2, $s3  
-        sub $s3, $s3, $s2  
-        sub $s2, $s3, $s2  
-  
-swapx:  slt $t1, $s2, $s0  
-        bne $0, $t1, doswapx  
-        j getdelta  
-# swap (x0 , x1) (y0, y1)  
-doswapx: add $s0, $s0, $s2  
-    sub $s2, $s2, $s0  
-    sub $s0, $s2, $s0  
-  
-    add $s1, $s1, $s3  
-    sub $s3, $s3, $s1  
-    sub $s1, $s3, $s1  
-getdelta: sub $s5, $s0, $s2  
-        sub $a0, $s1, $s3  
-        addi $sp, $sp, -1  
-        sw $ra, 0($sp)  
-        jal abs  
-        lw $ra, 0($sp)  
-        addi $sp, $sp, 1  
-        add $s6, $v0, $0  
-# y = y0  
-        add $t1, $s1, $0  
-# x = x0  
-        add $s7, $0, $0  
-        add $t0, $s0, $0  
-        slt $t3, $s1, $s3  
-        bne $t3, $0, pos   
-        addi $t2, $0, -1  
-        j init  
-  
-pos: addi $t2, $0, 1  
-init: addi $s2, $s2, 1  
-loop2: bne $s4, $0, rev  
-    add $a0, $t0, $0  
-    add $a1, $t1, $0  
-    addi $sp, $sp, -1  
-    sw $ra, 0($sp)  
-    jal plot  
-    lw $ra, 0($sp)  
-    addi $sp, $sp, 1  
-    j error  
-rev: add $a1, $t0, $0  
-    add $a0, $t1, $0  
-    addi $sp, $sp, -1  
-    sw $ra, 0($sp)  
-    jal plot  
-    lw $ra, 0($sp)  
-    addi $sp, $sp, 1  
-  
-error: add $s7, $s7, $s6  
-    add $t4, $s7, $s7 # 2 * error  
-    addi $t4, $t4, 1  
-    slt $t5, $s5, $t4 #2*error >= deltax  
-    bne $0, $t5, skip  
-    add $t1, $t1, $t2  
-    sub $s7, $s5, $s7  
-#loop check  
-skip: slt $t5, $t0, $s2  
-    addi $t0, $t0, 1  
-    bne $0, $t5, finish  
-    j loop2  
-finish: jr $ra  
-  
+plot: sw $a0, 0($t9)  #
+    sw $a1, 1($t9)   #
+    addi $t9, $t9, 2 #  
+    jr $ra  #
+abs:    add $v0, $0, $a0  #
+        slt $t8, $a0, $0  #
+        beq $t8, $0, jumpTo  #
+        sub $v0, $v0, $0   #
+jumpTo: jr $ra    #
+line: add $s0, $0, $a0  #first copy over all the args#
+    add $s1, $0, $a1  #
+    add $s2, $0, $a2  #
+    add $s3, $0, $a3  #
+    sub $t1, $s1, $s3  #calculate dy#
+    add $a0, $0, $t1  #load dy for abs#
+    addi $sp, $sp, -1  #
+    sw $ra, 0($sp)  #
+    jal abs  #
+    lw $ra, 0($sp)  #
+    addi $sp, $sp, 1  #
+    add $t1, $0, $v0  # save the value of v0
+    sub $t2, $s0, $s2  # and dx#
+    add $a0, $0, $t2  #
+    addi $sp, $sp, -1  #
+    sw $ra, 0($sp)  #
+    jal abs  #
+    lw $ra, 0($sp)  #
+    addi $sp, $sp, 1  #
+    add $t2, $0, $v0  #
+    slt $t0, $t2, $t1  # abs(dx) < abs(dy)
+    bne $0, $t0, setst  #
+    addi $s4, $0, 0   #
+    j swapxy  #
+setst: addi $s4, $0, 1   #
+swapxy: beq $0, $s4, swapx  #
+    add $s0, $s0, $s1  #
+    sub $s1, $s1, $s0  #
+    sub $s0, $s1, $s0  #
+    add $s2, $s2, $s3  #
+    sub $s3, $s3, $s2  #
+    sub $s2, $s3, $s2  #
+swapx:  slt $t1, $s2, $s0  # x1 < x0
+    beq $0, $t1, getdelta  #
+    add $s0, $s0, $s2  #
+    sub $s2, $s2, $s0  #
+    sub $s0, $s2, $s0  #
+    add $s1, $s1, $s3  #
+    sub $s3, $s3, $s1  #
+    sub $s1, $s3, $s1  #
+getdelta: sub $s5, $s0, $s2  #
+    sub $a0, $s1, $s3  #
+    addi $sp, $sp, -1  #
+    sw $ra, 0($sp)  #
+    jal abs  #
+    lw $ra, 0($sp)  #
+    addi $sp, $sp, 1  #
+    add $s6, $v0, $0  # set delta y
+    add $t1, $s1, $0  # setting y to y0
+    add $s7, $0, $0  # error?
+    add $t0, $s0, $0  # x = x0
+    slt $t3, $s1, $s3  # y0 < y1
+    beq $t3, $0, negyStep   #
+    addi $t2, $0, 1  #
+    j init  #
+negyStep: addi $t2, $0, -1  #
+init: addi $s2, $s2, 0  #
+loop2: bne $s4, $0, rev  # plot(x,y)
+    add $a0, $t0, $0  #
+    add $a1, $t1, $0  #
+    addi $sp, $sp, -1  #
+    sw $ra, 0($sp)  #
+    jal plot  #
+    lw $ra, 0($sp)  #
+    addi $sp, $sp, 1  #
+    j error  #
+rev: add $a1, $t0, $0  # plot (y,x)
+    add $a0, $t1, $0  #
+    addi $sp, $sp, -1  #
+    sw $ra, 0($sp)  #
+    jal plot  #
+    lw $ra, 0($sp)  #
+    addi $sp, $sp, 1  #
+error: add $s7, $s7, $s6  # error += dy
+    add $t4, $s7, $s7 # 2 * error 
+    addi $t4, $t4, 1  #
+    slt $t5, $s5, $t4 #2*error >= deltax 
+    beq $0, $t5, skip  #
+    add $t1, $t1, $t2  # y += ystep
+    sub $s7, $s5, $s7  # error -dx
+skip: slt $t5, $t0, $s2  # if(x < x1)
+    addi $t0, $t0, 1  # x++
+    bne $0, $t5, loop2  #
+finish: jr $ra  #
 close: add $s1, $0, $0  
   
